904cb209dd1958c061f49b4b2d66a150ac930ff2
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2021-12-20 13:24:55 +0700

    Add basic functionality to send image to the server

---
+++ app/src/main/java/com/example/litecctvvery/DBHelper.kt
@@ -0,0 +1,56 @@
+package com.example.litecctvvery
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+
+    // below is the method for creating a database by a sqlite query
+    override fun onCreate(db: SQLiteDatabase) {
+        // below is a sqlite query, where column names
+        // along with their data types is given
+        val query = ("CREATE TABLE " + TABLE_NAME + " ("
+                + ID_COL + " INTEGER PRIMARY KEY, " +
+                TOKEN_COL + " TEXT" + ")")
+
+        // we are calling sqlite
+        // method for executing our query
+        db.execSQL(query)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        // this method is to check if table already exists
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
+    }
+
+    // This method is for adding data in our database
+    fun addToken(token : String){
+        val values = ContentValues()
+        values.put(TOKEN_COL, token)
+        val db = this.writableDatabase
+        db.insert(TABLE_NAME, null, values)
+        db.close()
+    }
+
+    // below method is to get
+    // all data from our database
+    fun getToken(): Cursor? {
+        val db = this.readableDatabase
+        return db.rawQuery("SELECT token FROM " + TABLE_NAME, null)
+    }
+
+    companion object{
+        private val DATABASE_NAME = "Tokens"
+
+        // below is the variable for database version
+        private val DATABASE_VERSION = 1
+        val TABLE_NAME = "tokenTable"
+        val ID_COL = "id"
+        val TOKEN_COL = "token"
+    }
+}
--- app/src/main/java/com/example/litecctvvery/MainActivity.kt
+++ app/src/main/java/com/example/litecctvvery/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.litecctvvery
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.hardware.Camera
+import android.media.RingtoneManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -10,27 +13,54 @@ import android.os.Environment
 import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.util.Base64
 import android.util.Log
+import android.widget.Button
 import android.widget.FrameLayout
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private var TAG = "Main"
     private var mCamera: Camera? = null
     private var mPreview: CameraPreview? = null
+    private val motionDetector: MotionDetector = MotionDetector()
+    private val db = DBHelper(this, null)
+    private lateinit var token: String
+    private lateinit var tvToken: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvToken = findViewById(R.id.textview_token)
+
         if (!allPermissionsGranted()) {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
+
+        // Token generation
+        val db = DBHelper(this, null)
+        val cursor = db.getToken()
+        cursor!!.moveToFirst()
+        if(cursor.count != 0 ){
+            token = cursor.getString(0)
+            tvToken.setText(token)
+        }
+        else
+            generateToken()
 
         // Create an instance of Camera
         mCamera = getCameraInstance()
@@ -45,6 +75,15 @@ class MainActivity : AppCompatActivity() {
         mPreview?.also {
             val preview: FrameLayout = findViewById(R.id.camera_preview)
             preview.addView(it)
+        }
+
+        var captureButton: Button = findViewById(R.id.button_capture)
+        captureButton.setOnClickListener { captureButtonEvent() }
+    }
+
+    private fun captureButtonEvent() {
+        thread() {
+            mCamera?.takePicture(null, null, mPicture)
         }
     }
 
@@ -67,65 +106,102 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun generateToken() {
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET, URL_TOKEN_POST,
+            Response.Listener<String> { response ->
+                Log.e(TAG, "Generated Token: $response")
+                db.addToken(response)
+                token = response
+                tvToken.setText(token)
+            },
+            Response.ErrorListener { Log.e(TAG, "Token can NOT be generated") })
 
+        queue.add(stringRequest)
+    }
+
+    private fun playSound() {
+        try {
+            val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val r = RingtoneManager.getRingtone(applicationContext, notification)
+            r.play()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getBase64OfPhoto(image: Bitmap): String? {
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.PNG, 100, baos)
+        val byteArray: ByteArray = baos.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
+    }
+
+    private fun sendImageToCloud(fileName: String, imageData: String?, token: String) {
+        MySingleton.getInstance(this.applicationContext).requestQueue
+        val map = mutableMapOf<String, Any?>()
+        map["filename"] = fileName
+        map["token"] = token
+        map["imagedata"] = imageData
+
+        val json = JSONObject(map)
+        Log.e("sendImageToCloud", json.toString() )
+        val jsonReq = object: JsonObjectRequest(
+            Method.POST,URL_IMAGE_POST,json,
+            Response.Listener { response ->
+                Log.i(TAG, "Response from server: $response")
+            }, Response.ErrorListener{
+                // Error in request
+                Toast.makeText(this,
+                    "Volley error: $it",
+                    Toast.LENGTH_SHORT).show()
+            })
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json; charset=utf-8"
+                return headers
+            }
+        }
+
+        jsonReq.retryPolicy = DefaultRetryPolicy(
+            0,
+            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonReq)
+    }
 
     private val mPicture = Camera.PictureCallback { data, _ ->
-        val pictureFile: File = getOutputMediaFile(MEDIA_TYPE_IMAGE) ?: run {
-            Log.d(TAG, ("Error creating media file, check storage permissions"))
-            return@PictureCallback
-        }
+        var bitmap: Bitmap = BitmapFactory.decodeByteArray(data, 0, data.size)
+        if (motionDetector.hasMotion(bitmap)) {
+            Toast.makeText(baseContext, "MOTION DETECTED - Sending Picture To Server Now", Toast.LENGTH_LONG).show()
 
-        try {
-            val fos = FileOutputStream(pictureFile)
-            fos.write(data)
-            fos.close()
-        } catch (e: FileNotFoundException) {
-            Log.d(TAG, "File not found: ${e.message}")
-        } catch (e: IOException) {
-            Log.d(TAG, "Error accessing file: ${e.message}")
+            // Play sound
+            playSound()
+
+            // Resize bitmap
+            bitmap = Bitmap.createScaledBitmap(bitmap, IMAGE_CAPTURE_WIDTH, IMAGE_CAPTURE_HEIGHT, false)
+
+            val base64String = getBase64OfPhoto(bitmap)
+            val timestamp = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+            sendImageToCloud(timestamp, base64String, token)
         }
+        else {
+            Toast.makeText(baseContext, "NO MOTION DETECTED", Toast.LENGTH_LONG).show()
+        }
+        mCamera?.startPreview()
     }
 
-    private fun getOutputMediaFile(type: Int): File? {
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        val mediaStorageDir = File(
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-            "MyCameraApp"
-        )
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        mediaStorageDir.apply {
-            if (!exists()) {
-                if (!mkdirs()) {
-                    Log.d("MyCameraApp", "failed to create directory")
-                    return null
-                }
-            }
-        }
-
-        // Create a media file name
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        return when (type) {
-            MEDIA_TYPE_IMAGE -> {
-                File("${mediaStorageDir.path}${File.separator}IMG_$timeStamp.jpg")
-            }
-            else -> null
-        }
-    }
-
-
-    private fun getOutputMediaFileUri(type: Int): Uri {
-        return Uri.fromFile(getOutputMediaFile(type))
-    }
 
     companion object {
         private const val TAG = "CameraXBasic"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val IMAGE_CAPTURE_WIDTH = 300
+        private const val IMAGE_CAPTURE_HEIGHT = 300
+        private const val URL_IMAGE_POST = "http://128.199.123.139:8080/image/"
+        private const val URL_TOKEN_POST = "http://128.199.123.139:8080/tokenCheck/"
     }
 }
